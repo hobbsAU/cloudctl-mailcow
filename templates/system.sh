8cde70629d6cd0618c8b241b01dcf825e37d4082
hobbsAU <adrianhobbs@gmail.com>
2019-06-28 16:19:44 +0400

    updated aliases

--- templates/system.sh
+++ templates/system.sh
@@ -15,7 +15,7 @@ set -xeuo pipefail
 	# Setup aliases, prompts and fix ethernet
 	sed -i -e '/^#alias ll/s/^#//' /home/${SSH_USER}/.bashrc
 	sed -i -e '/^# export LS_OPTIONS/s/^#//' /root/.bashrc
-	sed -i -e '/^# alias ll/s/^#//' /root/.bashrc
+	sed -i -e '/^# alias ll/s/-l/-al/' -e '/^# alias ll/s/^#//' /root/.bashrc
 	echo "PS1='$${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >>/root/.bashrc
 
 	# Remove unwanted/conflicting packages
