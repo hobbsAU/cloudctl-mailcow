39182e4a0802976990eff3d1b5232e324856e6cd
hobbsAU <adrianhobbs@gmail.com>
2019-08-11 17:15:53 +0400

    removed weak SSL ciphers

--- templates/mailcow.sh
+++ templates/mailcow.sh
@@ -65,6 +65,9 @@ function Mailcow_Install() {
 	sed -i -E 's/^#IMAP_PORT=.*$|^IMAP_PORT=.*$/IMAP_PORT=127.0.0.1:143/g' ./mailcow.conf
 	sed -i -E 's/^#POP_PORT=.*$|^POP_PORT=.*$/POP_PORT=127.0.0.1:110/g' ./mailcow.conf
 
+	# Harden ciphers
+	sed -i 's/^\([[:blank:]]*\)ssl_cipher.*/\1ssl_ciphers \x27ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256\x27;/g' /opt/mailcow-dockerized/data/conf/nginx/site.conf
+
 	# Set umask
 	umask 0077
 }
